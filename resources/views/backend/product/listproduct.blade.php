cb5fbb51c8d7e39ba09f9cce87027494dea0b2ad
trandanhtan1234 <tantd@magezon.com>
2024-07-23 15:15:55 +0700

    test

--- resources/views/backend/product/listproduct.blade.php
+++ resources/views/backend/product/listproduct.blade.php
@@ -5,9 +5,11 @@
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
 	<div class="row">
 		<ol class="breadcrumb">
-			<li><a href="{{ url('admin') }}"><svg class="glyph stroked home">
+			<li>
+				<a href="{{ url('admin') }}"><svg class="glyph stroked home">
 						<use xlink:href="#stroked-home"></use>
-					</svg></a></li>
+					</svg></a>
+			</li>
 			<li class="active">Danh sách sản phẩm</li>
 		</ol>
 	</div>
