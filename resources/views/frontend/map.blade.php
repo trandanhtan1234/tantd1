923c8e52d68733641526b4e8699d4205766e99c4
trandanhtan1234 <64420471+trandanhtan1234@users.noreply.github.com>
2024-09-05 21:34:23 +0700

    Test Map

--- resources/views/frontend/map.blade.php
+++ resources/views/frontend/map.blade.php
@@ -16,7 +16,8 @@
 <body>
     <div class="container mt-5">
         <h2>Laravel - Google Maps</h2>
-        <div id="map"></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9243.364868170222!2d105.93874892983038!3d21.012518988733117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135a8c5b4f1deff%3A0x27e57979c5d3de97!2zQuG7h25oIHZp4buHbiDEkGEga2hvYSBHaWEgTMOibQ!5e1!3m2!1svi!2s!4v1725546065835!5m2!1svi!2s" width="full" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <!-- <div id="map"></div> -->
     </div>
   
     <script type="text/javascript">
